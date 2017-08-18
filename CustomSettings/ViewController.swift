3e5f49d6970776a862cd6b30cbf5afd8e6ce4281
Dick de Leeuw <dick@leeuwmedia.nl>
2017-08-18 14:12:00 +0200

    Preliminary dark mode implementation.

--- CustomSettings/ViewController.swift
+++ CustomSettings/ViewController.swift
@@ -21,7 +21,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         textView.becomeFirstResponder()
 
-        print("night mode setting is: \(UserDefaults.standard.bool(forKey: "nightMode"))")
+        print("initial night mode setting is: \(UserDefaults.standard.bool(forKey: "nightMode"))")
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.detectIfSettingsChanged), name: UserDefaults.didChangeNotification, object: nil)
+
+        //NotificationCenter.defaultCenter.addObserver(self, selector: #selector(self.detectIfSettingsChanged(_:)), name: "Setting", object: nil)
+
+    }
+
+
+    // MARK: - Functions
+
+    func detectIfSettingsChanged() {
+
+        let nightMode = nightModeIsActive()
+        let background = nightMode ? UIColor.black : UIColor.white
+        let fontColor = nightMode ? UIColor.white : UIColor.black
+
+        view.backgroundColor = background
+        textView.textColor = fontColor
+    }
+
+    func nightModeIsActive() -> Bool {
+        return UserDefaults.standard.bool(forKey: "nightMode") == true
     }
 
 }
