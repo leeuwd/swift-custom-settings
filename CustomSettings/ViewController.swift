b050818b0428f4f0a3853d9f5f15d233d3cb64f0
Dick de Leeuw <dick@leeuwmedia.nl>
2017-08-18 13:11:40 +0200

    Implemented text view UI.

--- CustomSettings/ViewController.swift
+++ CustomSettings/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+
+    @IBOutlet weak var textView: UITextView!
+
+
+    // MARK: - View did load
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textView.becomeFirstResponder()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
